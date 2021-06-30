6186fc6c31302b62e3bcd663498ddf5f6698de1d
Trxpxrx <trxpxrx@gmail.com>
2021-06-30 03:38:40 -0700

    se hizo el diseño del crear producto con sus estilos (faltan detalles) y se arreglo un error del carrito

--- src/src/routes/productsRouter.js
+++ src/src/routes/productsRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.index);
-router.get('/:id', productsController.productDetail);
+// router.get('/:id', productsController.productDetail);
 router.get('/crear', productsController.createProduct);
 router.get('/editar', productsController.editProduct);
 
