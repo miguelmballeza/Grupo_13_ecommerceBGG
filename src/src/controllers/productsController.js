468fc2f819551f9b4ad42bb3d7c391b5117a56bb
miguelmballeza <miguelballeza53@aragon.unam.mx>
2021-07-17 21:37:27 -0500

    changes

--- src/src/controllers/productsController.js
+++ src/src/controllers/productsController.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const productsPath = path.resolve(__dirname, '../data/products.json');
 const products = require(productsPath);
+
 // const fs = require('fs');
 // const productsString = fs.readFileSync(productsPath, {encoding : 'utf-8'});
 // const products = JSON.parse(productsString);
@@ -61,10 +62,10 @@ const productsController = {
     },
     
     agregar: function(req, res){
-        let id = products.length+1;
-        let datos = {id:id, title:req.body.album, artist:req.body.artist, year: parseInt(req.body.year), name:req.body.album, genre:req.body.genre, description:req.body.description, price:parseInt(req.body.price), IDContainer:req.body.color, IDImage:"standardImage", finalMessage:"Ir a la colección ->", image:"pleasepleaseme.jpg", songs: [{title: req.body.songs, length: req.body.lenght}]}
-        products.push()
-        res.send(datos)
+        let id = products.length;
+        const product = {id:id, title:req.body.album, artist:req.body.artist, year: parseInt(req.body.year), name:req.body.album, genre:req.body.genre, description:req.body.description, price: parseInt(req.body.price), IDContainer:req.body.color, IDImage:"standardImage", finalMessage:"Ir a la colección ->", image: req.file.filename, songs: [{title: req.body.songs, length: req.body.lenght}]}
+        products.push(JSON.parse(product));
+        res.redirect('products/productDetail', product);
     },
     
     updateProduct : function(req, res) {
@@ -85,7 +86,6 @@ const productsController = {
             product.image = req.body.image;
                 
             res.redirect('products/productDetail', { head, product});
-        
     }
 };
 
--- src/src/routes/productsRouter.js
+++ src/src/routes/productsRouter.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '.../public/images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now + "_img_" + path.extname(file.originalname));
+    }
+});
+
+const uploadFile = multer({ storage });
 
 router.get('/', productsController.index);
 router.get('/crear', productsController.createProduct);
-router.post('/crear', productsController.agregar )
+router.post('/crear', uploadFile.single('image'), productsController.agregar )
 router.get('/:id', productsController.productDetail);
 router.get('/:id/editar', productsController.editProduct);
 router.put('/:id', productsController.updateProduct );
