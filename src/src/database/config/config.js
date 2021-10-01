76f7ca5391b0d0dff42c7d5024b9236cceb7ec1e
Trxpxrx <trxpxrx@gmail.com>
2021-10-01 16:03:26 -0700

    prueba de la pagina

--- src/src/database/config/config.js
+++ src/src/database/config/config.js
@@ -22,5 +22,29 @@ module.exports = {
     "host": "127.0.0.1",
     "port": "3306",
     "dialect": "mysql"
-  }
+  },
+  "development-A": {
+    "username": "root",
+    "password": "",
+    "database": "vinylecommerce",
+    "host": "127.0.0.1",
+    "port": "3306",
+    "dialect": "mysql"
+  },
+  "test-A": {
+    "username": "root",
+    "password": "",
+    "database": "vinylecommerce",
+    "host": "127.0.0.1",
+    "port": "3306",
+    "dialect": "mysql"
+  },
+  "production-A": {
+    "username": "root",
+    "password": "",
+    "database": "vinylecommerce",
+    "host": "127.0.0.1",
+    "port": "3306",
+    "dialect": "mysql"
+  },
 };
