734efe20b2a9f8ae6e32f93e746544e2dd8618f0
miguelmballeza <miguelballeza53@aragon.unam.mx>
2021-10-09 11:02:22 -0500

    Modificación de archivos CSS para que se muestren correctamente los menus desplegables como el buscador y además se arreglaron unos colores.

---
+++ src/public/js/headerInteraction.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", () => {
+    const hamburguer = document.querySelector("div.hamburguer-button");
+    const menu = document.querySelector("section.menu");
+    const firstSection = document.querySelector("section.first-section");
+    const albumSection = document.querySelector("section.album-section");
+    const iconSearch = document.querySelector("button.search");
+    const searcher = document.querySelector("section.searcher");
+
+    hamburguer.addEventListener("click", () => {
+        menu.classList.toggle("menuClicked");
+    });
+
+    iconSearch.addEventListener("click", (event) => {
+        if(window.screen.width <= 991){
+            event.preventDefault();
+            searcher.classList.toggle("iconSearchClicked");
+        }
+    });
+
+
+    firstSection.addEventListener("click", () => {
+        menu.classList.remove("menuClicked");
+        searcher.classList.remove("iconSearchClicked");
+    });
+    albumSection.addEventListener("click", () => {
+        menu.classList.remove("menuClicked");
+        searcher.classList.remove("iconSearchClicked");
+    });
+});
